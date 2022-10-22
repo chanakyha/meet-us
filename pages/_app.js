760e3a42fe439b9de7ff991a04e5c393069a4d19
Chanakyha <vetrichanakyha2003@gmail.com>
2022-10-22 18:51:21 +0530

    Changed from TSX to JS

---
+++ pages/_app.js
@@ -0,0 +1,13 @@
+import "../styles/globals.css";
+import "antd/dist/antd.css";
+import AuthProvider from "../hooks/useAuth";
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <AuthProvider>
+      <Component {...pageProps} />
+    </AuthProvider>
+  );
+}
+
+export default MyApp;
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,8 @@
-import type { NextPage } from "next";
 import Head from "next/head";
+import { useAuth } from "../hooks/useAuth";
 
-const Home: NextPage = () => {
+const Home = () => {
+  const { user } = useAuth();
   return (
     <div className="flex min-h-screen flex-col items-center justify-center py-2">
       <Head>
